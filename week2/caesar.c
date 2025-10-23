abd3e2602fbe1640be164a916b600545584283b7
h3l0o <lansiemad@gmail.com>
2025-10-23 23:56:29 +0300

    Rewrote caesar.c making sure to brake bigger problems into smaller one and searching for info I didn't have :)

--- week2/caesar.c
+++ week2/caesar.c
@@ -1,47 +1,51 @@
-#include <assert.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char *cipher(char text[], int key);
+#define USAGE_ERROR 1;
 
-int main(int argc, char *argv[]) {
-
-  if (argc == 2 && isdigit(argv[1])) {
-    char text[20];
-    printf("Plain text: ");
-    scanf("%s", text);
-
-    cipher(text, (int)argv[1]);
-
-  } else {
-    printf("Usage: ./caesar.c key\n");
-    return 1;
-  }
-}
-
-char *cipher(char text[], int key) {
-
-  if (key < 0)
-    return "Key can't be a negative number";
-
-  for (int i = 0, length = strlen(text); i < length; i++) {
-
-    if (isalpha(text[i])) {
-
-      text[i] = tolower(text[i]);
-
-    } else
-      return "Plain text contains non-Alphabitic characters";
-  }
-
+char *cipher_text(char text[], int key) {
   static char cipher[20];
-
   for (int i = 0, length = strlen(text); i < length; i++) {
 
-    cipher[i] = (text[i] + key) % 26;
-    if (cipher[i] > 'z')
-      cipher[i] -= 25;
+    if (isupper(text[i]))
+      cipher[i] = ((text[i] - 'A' + key) % 26) + 'A';
+
+    else if (islower(text[i]))
+      cipher[i] = ((text[i] - 'a' + key) % 26) + 'a';
+
+    else if (isspace(text[i]))
+      cipher[i] = ' ';
+
+    else
+      cipher[i] = text[i];
   }
   return cipher;
+}
+
+int main(int argc, char *argv[]) {
+  char text[20];
+
+  if (argc == 1 || argc > 2) {
+    printf("Usage ./ceasar key");
+    return USAGE_ERROR;
+  }
+  int key = atoi(argv[1]);
+
+  //********Error checking********
+
+  for (int i = 0, n = strlen(argv[1]); i < n; i++) {
+    if (!isdigit(argv[1][i])) {
+      printf("Usage ./ceasar key");
+      return USAGE_ERROR;
+    }
+  }
+
+  //******************************
+
+  printf("Plain text: ");
+  fgets(text, 20, stdin);
+
+  printf("Cipher text: %s", cipher_text(text, key));
 }
